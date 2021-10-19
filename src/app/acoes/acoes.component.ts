73ceefa58a594407da7b8d87776d0bd9f4883541
Matheus Mastrocinque <tec.mastrocinque@gmail.com>
2021-10-19 13:57:57 -0300

    Testing rxjs operators

---
+++ src/app/acoes/acoes.component.ts
@@ -0,0 +1,23 @@
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { FormControl } from '@angular/forms';
+import { Acoes } from './modelo/acoes';
+import { AcoesService } from './acoes.service';
+import { Subscription } from 'rxjs';
+import { switchMap, tap } from 'rxjs/operators';
+
+@Component({
+  selector: 'app-acoes',
+  templateUrl: './acoes.component.html',
+  styleUrls: ['./acoes.component.css'],
+})
+export class AcoesComponent {
+  acoesInput = new FormControl();
+  acoes$ = this.acoesInput.valueChanges.pipe(
+    tap(console.log),
+    switchMap((valorDigitado) => 
+      this.acoesService.getAcoes(valorDigitado)),
+      tap(console.log)
+    );
+
+  constructor(private acoesService: AcoesService) {}
+}
